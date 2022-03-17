35d4b096784c0bf0dd509580f6272503550cd8f4
lMertht <101404980+lvalrak@users.noreply.github.com>
2022-03-17 18:58:06 -0300

    Add files via upload

---
+++ ABPro-M3-AE1-New-formulario/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("#nav-placeholder").load("nav.html")
+  })
+
+/*  $(function(){
+    $("#segundo-placeholder").load("segundo.html")
+  })
+
+  $(function(){
+    $("#tercer-placeholder").load("tercer.html")
+  })
+
+  $(function(){
+    $("#cuarto-placeholder").load("cuarto.html")
+  })
+
+  $(function(){
+    $("#quinto-placeholder").load("quinto.html")
+  })
+
+  $(function(){
+    $("#sexto-placeholder").load("sexto.html")
+  })*/
